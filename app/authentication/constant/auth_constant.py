09a30c6e1bc7f999ba008b7f850e6726a0ac839e
Masud Rana <masud.cseian@gmail.com>
2023-10-06 02:26:41 +0600

    Remove extra constant

--- app/authentication/constant/auth_constant.py
+++
@@ -1,6 +0,0 @@
-REGISTRATION_EMAIL_SUBJECT = 'Conform Your Registration'
-REGISTRATION_EMAIL_BODY = """
-<p>Welcome, Please confirm your email by clicking this link:</p>
-<p><a href="{}">Click here to confirm</a></p>
-"""
-
